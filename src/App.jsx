366a544630cadb44bf87a167f7a6a97d7e60e09e
unknown <nirajandangal8@gmail.com>
2026-08-19 08:50:35 +0545

    import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import UserList from "./pages/UserList";
